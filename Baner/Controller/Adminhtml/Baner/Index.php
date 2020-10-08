af6e13490995ab63271f165e932a06ede13444e1
kamilgwozdziowski <admin@2.pl>
2020-10-08 17:53:30 +0200

    init panel admin

---
+++ Baner/Controller/Adminhtml/Baner/Index.php
@@ -0,0 +1,48 @@
+<?php
+namespace MylSoft\Baner\Controller\Adminhtml\Baner;
+
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'MylSoft_Baner::baner';
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Registry $coreRegistry
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('MylSoft_baner::all_baner')
+            ->addBreadcrumb(__('All Baner'), __('All Baner'))
+            ->addBreadcrumb(__('Baner'), __('Baner'))
+            ->getConfig()->getTitle()->prepend(__('All Baner'));
+
+        return $resultPage;
+    }
+}
