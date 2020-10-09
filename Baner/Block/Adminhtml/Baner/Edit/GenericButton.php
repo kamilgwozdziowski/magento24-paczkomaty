5fa0cbd5ac14a9487ae6e70972e26a1f99daa7dd
kamilgwozdziowski <admin@2.pl>
2020-10-09 14:28:14 +0200

    panel formularze dodanie przycisków

---
+++ Baner/Block/Adminhtml/Baner/Edit/GenericButton.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MylSoft\Baner\Block\Adminhtml\Baner\Edit;
+
+use Magento\Backend\Block\Widget\Context;
+
+/**
+ * Class GenericButton
+ */
+class GenericButton
+{
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    /**
+     * @param Context $context
+     */
+    public function __construct(
+        Context $context
+    ) {
+        $this->context = $context;
+    }
+
+    /**
+     * Return Baner ID
+     *
+     * @return int|null
+     */
+    public function getBanerId()
+    {
+        return (int)$this->context->getRequest()->getParam('id');
+    }
+
+    /**
+     * Generate url by route and parameters
+     *
+     * @param   string $route
+     * @param   array $params
+     * @return  string
+     */
+    public function getUrl($route = '', $params = [])
+    {
+        return $this->context->getUrlBuilder()->getUrl($route, $params);
+    }
+}
