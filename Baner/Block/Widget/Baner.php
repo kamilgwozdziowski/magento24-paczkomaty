8eef65e3722d6b7e638ef2ceabb5a27f7734f6bf
kamilgwozdziowski <admin@2.pl>
2020-10-18 16:19:01 +0200

    wyswietlanie widgeta

---
+++ Baner/Block/Widget/Baner.php
@@ -0,0 +1,9 @@
+<?php
+namespace MylSoft\Baner\Block\Widget;
+
+use Magento\Widget\Block\BlockInterface;
+
+class Baner extends \Magento\Framework\View\Element\Template implements BlockInterface
+{
+    protected $_template = "widget/baner.phtml";
+}
