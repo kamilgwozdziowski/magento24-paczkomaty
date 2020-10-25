07bf88ff9a47a1f0f8ceec7ae74e28cb6e82ea33
kamilgwozdziowski <admin@2.pl>
2020-10-25 20:36:18 +0100

    install new atribiutte

---
+++ Paczkomaty/Setup/InstallData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MylSoft\Paczkomaty\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Config;
+use Magento\Customer\Model\Customer;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->eavConfig = $eavConfig;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'paczkomat',
+            [
+                'type' => 'varchar',
+                'label' => 'Default paczkomat for user',
+                'input' => 'text',
+                'required' => false,
+                'visible' => true,
+                'user_defined' => true,
+                'position' => 999,
+                'system' => 0,
+            ]
+        );
+        $sampleAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'paczkomat');
+
+        $sampleAttribute->setData(
+            'used_in_forms',
+            ['adminhtml_customer']
+
+        );
+        $sampleAttribute->save();
+    }
+}
